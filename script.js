79b926289d967317b0bc4474a540d6364a18b7a4
Jake Bray <jakebray@Megans-MacBook-Pro.local>
2017-11-14 08:22:15 -0600

    completed code 1 challenge

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ $(document).ready(onReady);
 
 function onReady() {
     $('#generateButton').on('click', addDiv);
-    $()
-    $('.multipleDivs').on('click', '.swapButton', swapButton);
-    $('.multipleDivs').on('click', '.deleteButton', deleteButton);
+    $('body').on('click', '.swapButton', swapButton);
+    $('body').on('click', '.deleteButton', deleteButton);
 }
+
+var clicks = 0
 
 function addDiv(){
-    $('body').append('<div class="multipleDivs"></div>');
-    $('.multipleDivs').append('<p id="buttonClicks"></p>');
-    $('#buttonClicks').html(function(i, val) { return val*1+1 }).append(val);
-    var val = 0;   
-    $('#buttonClicks').append('<button>Swap</button>');
+    clicks += 1    
+    $('body').append('<div class="multipleDivs"><p class="buttonClicks">' + clicks + '</p><button class="swapButton">Swap</button><button class="deleteButton">Delete</button></div>');
 }
 
-function addButtons() {
-    $('.multipleDivs').append('<button id="swapButton">Swap</button>')
-    $('.multipleDivs').append('<button id="deleteButton">Delete</button>')
+
+function swapButton() {
+    $(this).parent().toggleClass('yellow');
 }
 
-// function swapButton() {
-//     $('#swapButton').css('background-color', 'yellow');
-// }
-
-// function deleteButton() {
-    //$(this).parent().remove();
-// }
+function deleteButton() {
+    $(this).parent().remove();
+}
